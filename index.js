a145e935c13d68cbc5bd71039c1807f1041a3f99
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-06-26 22:24:30 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.querySelector('main#main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'J is the champion.';
+document.body.append('h1');
